780bc8e52af7168a76a7a451fd56caea3bc8db43
Darren Walker <darrenwalker1@hotmail.co.uk>
2024-07-09 09:57:25 +0100

    add 'fetchObservatory' command, move ObjectMapper and ProposalRestAPI objects into 'top-command', use '@ParentCommand' annotated PolarisCLI object in subcommands.

--- jvm/src/main/java/org/orph2020/pst/cli/FetchPeople.java
+++ jvm/src/main/java/org/orph2020/pst/cli/FetchPeople.java
@@ -1,29 +1,20 @@
 package org.orph2020.pst.cli;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.enterprise.context.Dependent;
-import jakarta.inject.Inject;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.orph2020.pst.apiimpl.client.ProposalRestAPI;
-import picocli.CommandLine;
+import picocli.CommandLine.*;
 
-@Dependent
-@CommandLine.Command(name="fetchPeople")
+@Command(name="fetchPeople")
 public class FetchPeople implements Runnable{
 
-    @Inject
-    protected ObjectMapper mapper;
-
-    @RestClient
-    ProposalRestAPI api;
+    @ParentCommand
+    private PolarisCLI parent;
 
 
     @Override
     public void run() {
         try {
             System.out.println("Fetching people:");
-            System.out.println(mapper.writeValueAsString(api.getPeople()));
+            System.out.println(parent.mapper.writeValueAsString(parent.api.getPeople()));
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
--- jvm/src/main/java/org/orph2020/pst/cli/PolarisCLI.java
+++ jvm/src/main/java/org/orph2020/pst/cli/PolarisCLI.java
@@ -1,31 +1,39 @@
 package org.orph2020.pst.cli;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.orph2020.pst.apiimpl.client.ProposalRestAPI;
 import picocli.CommandLine;
+import picocli.CommandLine.*;
 
 @QuarkusMain
-@CommandLine.Command( mixinStandardHelpOptions = true,
-        subcommands = {FetchPeople.class, FetchProposal.class})
+@Command(
+        mixinStandardHelpOptions = true,
+        subcommands = {
+                FetchPeople.class,
+                FetchProposal.class,
+                FetchObservatory.class
+        })
 public class PolarisCLI implements QuarkusApplication {
 
-    @CommandLine.Option(names = {"-u","--user"})
+    @Option(names = {"-u","--user"})
     String user;
 
-    @CommandLine.Option(names = {"-p","--password"}, arity = "0..1", interactive = true)
+    @Option(names = {"-p","--password"}, arity = "0..1", interactive = true)
     String password;
 
     @Inject
-    CommandLine.IFactory factory;
+    IFactory factory;
 
     @Inject
-    FetchPeople fetchPeople;
+    ObjectMapper mapper;
 
-    @Inject
-    FetchProposal fetchProposal;
-
+    @RestClient
+    ProposalRestAPI api;
 
     @Override
     public int run(String... args) throws Exception {
