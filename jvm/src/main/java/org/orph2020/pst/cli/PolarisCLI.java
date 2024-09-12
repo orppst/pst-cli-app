f9a44d805db1ae8a91ba905a958447f7417baff6
Allan England <allan.england@manchester.ac.uk>
2024-09-12 10:40:54 +0100

    WIP: Rudamentry device flow auth

--- jvm/src/main/java/org/orph2020/pst/cli/PolarisCLI.java
+++ jvm/src/main/java/org/orph2020/pst/cli/PolarisCLI.java
@@ -2,13 +2,30 @@ package org.orph2020.pst.cli;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.oidc.client.OidcClient;
+import io.quarkus.oidc.common.runtime.OidcConstants;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jose4j.json.internal.json_simple.JSONObject;
+import org.jose4j.json.internal.json_simple.parser.JSONParser;
+import org.jose4j.json.internal.json_simple.parser.ParseException;
 import org.orph2020.pst.apiimpl.client.ProposalRestAPI;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Thread.sleep;
 
 @QuarkusMain
 @Command(
@@ -29,6 +46,9 @@ public class PolarisCLI implements QuarkusApplication, Runnable {
     @Inject
     ObjectMapper mapper;
 
+    @Inject
+    OidcClient oidcClient;
+
     @RestClient
     ProposalRestAPI api;
 
@@ -42,10 +62,62 @@ public class PolarisCLI implements QuarkusApplication, Runnable {
     public void run() {
         System.out.println("Running Polaris CLI");
 
-        System.out.println("I need to use the OidcClient.accessTokens \n"
-                +"method to accept a Map of extra properties and pass the \n"
-                +"device_code and redirect_uri parameters to exchange the \n"
-                +"authorization code for the tokens.");
+        String device_code = "";
+        String verification_uri_complete = "";
+
+        // Generate a device code
+        HttpClient httpClient = HttpClient.newHttpClient();
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:53536/realms/orppst/protocol/openid-connect/auth/device"))
+                .POST(HttpRequest.BodyPublishers.ofString("client_id=device-flow-client&client_secret=iEoMVJqKeCK9Wq05eKaZscFMdnjRLq3B"))
+                .setHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        try {
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+            JSONParser parser = new JSONParser();
+            JSONObject jsonResponse = (JSONObject) parser.parse(response.body());
+
+            device_code = (String) jsonResponse.get("device_code");
+            verification_uri_complete = (String) jsonResponse.get("verification_uri_complete");
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("HTTP POST error: " + e.getMessage());
+        } catch (ParseException e) {
+            System.out.println("Unable to parse response: " + e.getMessage());
+        }
+
+        // Open browser for credentials
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(verification_uri_complete));
+            } catch (IOException | URISyntaxException e) {
+                System.out.println("Unable to open browser: " + e.getMessage());
+            }
+        }else{
+            System.out.println("Desktop is not supported");
+        }
+
+        // Use device code to get an auth token
+        Map<String, String> grantParams = new HashMap<>();
+        grantParams.put("device_code", device_code);
+        String encodedIdToken = "";
+
+
+        try {
+            sleep(10000);
+
+            encodedIdToken = oidcClient.getTokens(grantParams).await().indefinitely().get("access_token");
+            System.out.println("Got this..." + encodedIdToken);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
 
     }
 }
