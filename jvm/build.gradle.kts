7235cc3a78a7969ab14b10f47fa10360acb06e93
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-10-14 07:45:31 +0100

    upgrade to quarkus 3.13.3

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
 
-   id("io.quarkus") version "3.12.2"
+   id("io.quarkus") version "3.13.3"
 }
 
 dependencies {
    implementation(platform("org.orph2020.pst.platforms:quarkus-base"))
    implementation(platform("org.orph2020.pst.platforms:pst-base"))
    implementation("org.orph2020.pst:pst-lib")
-   implementation("io.quarkus:quarkus-oidc-client")
    implementation("io.quarkus:quarkus-picocli")
    implementation("io.quarkus:quarkus-rest-client-reactive-jackson")
+   implementation("io.quarkus:quarkus-oidc-client-reactive-filter")
    testImplementation("io.quarkus:quarkus-junit5")
    testImplementation("io.rest-assured:rest-assured")
 
