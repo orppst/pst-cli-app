7a6ade4ba0762e423f9581d6f40749b19717a534
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-03-17 12:57:03 +0000

    update to quarkus 3.32.3
    * also update the gradle wrapper to 9.4
    * remove some of the build logic platform complexity (at the expense of a bit more repetition perhaps)

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
 
-   id("io.quarkus") version "3.13.3"
+   id("io.quarkus") version "3.32.3"
 }
 
 dependencies {
-   implementation(platform("org.orph2020.pst.platforms:quarkus-base"))
-   implementation(platform("org.orph2020.pst.platforms:pst-base"))
+   implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.32.3"))
    implementation("org.orph2020.pst:pst-lib")
    implementation("io.quarkus:quarkus-picocli")
-   implementation("io.quarkus:quarkus-rest-client-reactive-jackson")
-   implementation("io.quarkus:quarkus-oidc-client-reactive-filter")
+   implementation("io.quarkus:quarkus-rest-client-jackson")
+   implementation("io.quarkus:quarkus-rest-client-oidc-filter")
    testImplementation("io.quarkus:quarkus-junit5")
    testImplementation("io.rest-assured:rest-assured")
 
